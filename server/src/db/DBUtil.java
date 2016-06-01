6b86a2fd5132bf6717c2a357819d6a7d76175653
lbr0307 <lbr0307@126.com>
2016-05-31 23:03:49 -0400

    change the port number from 3306 to 10000.

--- server/src/db/DBUtil.java
+++ server/src/db/DBUtil.java
@@ -2,6 +2,7 @@ package db;
 
 public class DBUtil {
 	private static final String HOSTNAME = "localhost";
+	// Change the port number from default
 	private static final String PORT_NUM = "10000";
 	private static final String USERNAME = "root";
 	private static final String PASSWORD = "root";
